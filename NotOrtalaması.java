f1b2411e904497f764667153bcc5b4509b7256e1
busetozlu <busetozlu@outlook.com>
2022-10-11 15:53:54 +0300

    secondcommit

--- NotOrtalaması.java
+++ NotOrtalaması.java
@@ -1,4 +1,28 @@
 import java.util.Scanner;
 public class NotOrtalaması {
-    int mat,fizik;
+    public static void main(String[]args) {
+        int mat,fizik,kimya,turkce, tarih, muzik;
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Matematik Notunuz:");
+        mat=inp.nextInt();
+        System.out.print("Fizik Notunuz:");
+        fizik=inp.nextInt();
+        System.out.print("Kimya Notunuz:");
+        kimya=inp.nextInt();
+        System.out.print("Türkçe Notunuz:");
+        turkce=inp.nextInt();
+        System.out.print("Tarih Notunuz:");
+        tarih=inp.nextInt();
+        System.out.print("Müzik Notunuz:");
+        muzik=inp.nextInt();
+        int toplam=(mat+fizik+kimya+turkce+tarih+muzik);
+        double ortalama=toplam/6;
+        System.out.println("Ortalamanız: "+ortalama);
+
+        boolean gecti=(ortalama>60);
+        boolean kaldı=(ortalama<=60);
+        String durum=(gecti)? "Sınıfı Geçti":"Sınıfta Kaldı";
+        System.out.print(durum);
+
+    }
 }
