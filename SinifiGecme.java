fff628e4f29a7aa40bf4f1322e37e3700dd4d5de
busetozlu <busetozlu@outlook.com>
2022-10-13 19:55:41 +0300

    secondcommit

--- SinifiGecme.java
+++ SinifiGecme.java
@@ -3,27 +3,63 @@ public class SinifiGecme {
     public static void main (String[]args){
         int mat,fizik,turkce,kimya,muzik;
         double ortalama;
+        int sayac=0,toplamnot=0;
         Scanner inp=new Scanner(System.in);
         System.out.print("Matematik Notu:");
         mat=inp.nextInt();
+        if (mat<0 || mat>100){
+            System.out.println("'0' ve '100' aralığında olmayan notlar ortalamaya dehil edilemez.");
+        }
+        else {
+            sayac+=1;
+            toplamnot+=mat;
+        }
         System.out.print("Fizik Notu:");
         fizik=inp.nextInt();
+        if (fizik<0 || fizik>100){
+            System.out.println("'0' ve '100' aralığında olmayan notlar ortalamaya dehil edilemez.");
+        }
+        else {
+            sayac+=1;
+            toplamnot+=fizik;
+        }
         System.out.print("Türkçe Notu:");
         turkce=inp.nextInt();
+        if (turkce<0 || turkce>100){
+            System.out.println("'0' ve '100' aralığında olmayan notlar ortalamaya dehil edilemez.");
+
+        }
+        else {
+            sayac+=1;
+            toplamnot+=turkce;
+        }
         System.out.print("Kimya Notu:");
         kimya=inp.nextInt();
+        if (kimya<0 || kimya>100){
+            System.out.println("'0' ve '100' aralığında olmayan notlar ortalamaya dehil edilemez.");
+
+        }
+        else {
+            sayac+=1;
+            toplamnot+=kimya;
+        }
         System.out.print("Müzik Notu:");
         muzik=inp.nextInt();
-        if (mat!=0 && mat!=100 && fizik!=0 && fizik!=100 && turkce!=0 && turkce!=100 && kimya!=0 && kimya!=100 && muzik!=0 && muzik!=100 ){
-            ortalama=(mat+fizik+turkce+kimya+muzik)/5.0;
-            System.out.println("Ortalama:"+ortalama);
-            if (ortalama>55)
-            {
-                System.out.println("Sınıfı Geçti");
-            }
-            else{
-                System.out.println("Sınıfta Kaldı");
-            }
+        if (muzik<0 || muzik>100){
+            System.out.println("'0' ve '100' aralığında olmayan notlar ortalamaya dehil edilemez.");
+        }
+        else {
+            sayac+=1;
+            toplamnot+=muzik;
+        }
+        ortalama=(toplamnot/sayac);
+        if (ortalama>55)
+        {
+            System.out.println("Sınıfı geçtiniz, ortalamanız:"+ortalama);
+        }
+        else
+        {
+            System.out.println("Sınıfta kaldınız, ortalamanız:"+ortalama);
         }
 
 
