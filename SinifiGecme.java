0557ab15a7befb94e1c0a04b6abdd559c938a0cc
busetozlu <busetozlu@outlook.com>
2022-10-13 12:17:38 +0300

    secondcommit

--- SinifiGecme.java
+++ SinifiGecme.java
@@ -17,7 +17,7 @@ public class SinifiGecme {
         if (mat!=0 && mat!=100 && fizik!=0 && fizik!=100 && turkce!=0 && turkce!=100 && kimya!=0 && kimya!=100 && muzik!=0 && muzik!=100 ){
             ortalama=(mat+fizik+turkce+kimya+muzik)/5.0;
             System.out.println("Ortalama:"+ortalama);
-            if (ortalama>=55)
+            if (ortalama>55)
             {
                 System.out.println("Sınıfı Geçti");
             }
