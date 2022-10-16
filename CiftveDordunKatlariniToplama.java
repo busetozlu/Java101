c44b1f12a8222a47a857d20b99b93ba52c8003c9
busetozlu <busetozlu@outlook.com>
2022-10-16 07:58:24 +0300

    firstcommit

---
+++ CiftveDordunKatlariniToplama.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class CiftveDordunKatlariniToplama {
+    public static void main(String[]args){
+       int i,total=0;
+        Scanner input=new Scanner(System.in);
+       do {
+           System.out.print("Sayı giriniz:");
+           i= input.nextInt();
+           if(i % 2==0 && i%4==0) {
+               total+=i;
+           }
+       }while (i%2==0);
+        System.out.println("Girilen çift ve dördün katları " +
+                            "olan sayıların toplamı: "+total);
+    }
+
+}
