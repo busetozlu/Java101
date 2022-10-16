2857716e684502d5939e3b45afe33b241b031b3f
busetozlu <busetozlu@outlook.com>
2022-10-16 10:47:03 +0300

    firstcommit

---
+++ DortBesKuvvetleriniYazdırma.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class DortBesKuvvetleriniYazdırma {
+    public static void main(String[]args){
+        int i,number;
+        Scanner input=new Scanner(System.in);
+        System.out.print("Sınır sayısını giriniz:");
+        number=input.nextInt();
+        System.out.println("4'ün Kuvvetleri;");
+        for (i=1; i<=number; i*=4){
+            System.out.println(i);
+        }
+        System.out.println("5'in Kuvvetleri:");
+        for (i=1; i<=number; i*=5){
+            System.out.println(i);
+        }
+    }
+}
