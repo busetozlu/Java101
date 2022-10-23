dd1b5bd7c0a7c429bcbe49b2cb2d5a6931fc81d7
busetozlu <busetozlu@outlook.com>
2022-10-23 14:15:52 +0300

    firstcommit

---
+++ FibonacciMetot.java
@@ -0,0 +1,12 @@
+public class FibonacciMetot {
+    static int fib(int n){
+        if (n==1 || n==0){
+            return 1;
+        }
+        return fib(n-1)+fib(n-2);
+
+    }
+    public static void main (String[]args){
+        System.out.println(fib(5));
+    }
+}
